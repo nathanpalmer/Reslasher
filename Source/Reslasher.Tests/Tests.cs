0df78385804cf049d283d6edc014a92b67fc2172
Nathan Palmer <nate@thinktheweb.com>
2012-05-31 19:08:54 -0500

    Initial commit that includes Reslash and Combine

---
+++ Source/Reslasher.Tests/Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Reslasher.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Does_It_Slash_Properly()
+        {
+            // Arrange
+            var input = @"C:\Folder\Root";
+
+            // Act
+            input = input.Reslash();
+
+            // Assert
+            input.ShouldBe("C:/Folder/Root");
+        }
+
+        [Test]
+        public void Can_It_Slash_The_Other_Way()
+        {
+            // Arrange
+            var input = @"C:/Folder/Root";
+
+            // Act
+            input = input.Reslash('/','\\');
+
+            // Assert
+            input.ShouldBe(@"C:\Folder\Root");
+        }
+
+        [Test]
+        public void Mixed_Slashes_Get_Normalized()
+        {
+            // Arrange
+            var input = @"C:\Folder/Root";
+
+            // Act
+            input = input.Reslash();
+            
+            // Assert
+            input.ShouldBe("C:/Folder/Root");
+        }
+
+        [Test]
+        public void Can_Combine_Path_With_Root()
+        {
+            // Arrange
+            var input = @"C:\Root\Path";
+            var next = @"/Other/Path";
+
+            // Act
+            var result = input.Combine(next);
+
+            // Assert
+            result.ShouldBe(@"C:\Root\Path\Other\Path");
+        }
+
+        [Test]
+        public void Can_Combine_Multiple_Paths()
+        {
+            // Arrange
+            var input = @"C:\Root\Path";
+            var next1 = @"Other";
+            var next2 = @"Moar\Path";
+
+            // Act
+            var result = input.Combine(next1, next2);
+
+            // Assert
+            result.ShouldBe(@"C:\Root\Path\Other\Moar\Path");
+        }
+    }
+}
---
+++ Source/Reslasher/ReslashExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Reslasher
+{
+    public static class ReslashExtensions
+    {
+        public static string Reslash(this string input, char from = '\\', char to = '/')
+        {
+            return input.Replace(from, to);
+        }
+
+        public static string Combine(this string input, params string[] next)
+        {
+            var firstSlash = input.IndexOfAny(new[] {'/', '\\'});
+            var from = firstSlash >= 0
+                           ? input[firstSlash] == '\\'
+                                 ? '/'
+                                 : '\\'
+                           : '\\';
+            var to = from == '/' ? '\\' : '/';
+            var result = input;
+
+            foreach (var s in next)
+            {
+                var item = s.Reslash(from, to);
+                item = item[0] == to ? item.Substring(1) : item;
+                result = Path.Combine(result, item);
+            }
+
+            return result.Reslash(from, to);
+        }
+    }
+}
